838b511887acda5cbfd28f590cf540854124b8d4
marison17 <mleversen17@gmail.com>
2016-08-03 15:02:22 -0500

    Update led_jamm.ino

--- led_jamm.ino
+++ led_jamm.ino
@@ -11,17 +11,13 @@ void loop() {
   digitalWrite(4, HIGH);
   delay(200);
   digitalWrite(4, LOW);
-  delay(600); //If doesn't work, take out delay(600); and just keep it on low until the loop repeats
   digitalWrite(5, HIGH);
   delay(200);
   digitalWrite(5, LOW);
-  delay(600);
   digitalWrite(6, HIGH);
   delay(200);
   digitalWrite(6, LOW);
-  delay(600);
   digitalWrite(7, HIGH);
   delay(200);
   digitalWrite(7, LOW);
-  delay(600);
 }
