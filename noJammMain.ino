9f19882fcad4beadde21aacd6e76cc604879ee9f
aniaHrrera4 <aniaherrera02@gmail.com>
2016-08-03 15:01:20 -0500

    Update noJammMain.ino

--- noJammMain.ino
+++ noJammMain.ino
@@ -1,9 +1,29 @@
+#include 
+#include 
+
+int PI=3.14;
+
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
+  Generator(pin, OUTPUT);
+  Transmeter(pin, INPUT);
+  button1(off, LOW);
+  button2(on, HIGH);
 
 }
 
+function GenSineWave(){
+  y=Asin(2piF+phase shift)
+  
+  }
 void loop() {
   // put your main code here, to run repeatedly:
+  while(button1==ON){}
+
+      GenSineWave();
 
 }
