e87aecbf3fbf4982855009078dca8c3c62af647b
marison17 <mleversen17@gmail.com>
2016-08-05 11:45:35 -0500

    Update led_jamm.ino

--- led_jamm.ino
+++ led_jamm.ino
@@ -1,23 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(4, OUTPUT); //red
-  pinMode(5, OUTPUT); //green
-  pinMode(6, OUTPUT); //blue
-  pinMode(7, OUTPUT); //yellow
+  pinMode(3.3, OUTPUT); //plugged into 3.3V
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(4, HIGH);
-  delay(200);
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-  delay(200);
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  delay(200);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  delay(200);
-  digitalWrite(7, LOW);
+  digitalWrite(3.3, HIGH);
+  delay(2000);
+  digitalWrite(3.3, LOW);
+  delay(2000);
 }
