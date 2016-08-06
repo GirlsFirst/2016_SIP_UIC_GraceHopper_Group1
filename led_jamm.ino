7997d7fba310a73ee0238b76896e0bd9f71c0b23
aniaHrrera4 <aniaherrera02@gmail.com>
2016-08-05 22:45:11 -0500

    Update led_jamm.ino

--- led_jamm.ino
+++ led_jamm.ino
@@ -1,4 +1,4 @@
-void setup() {
+/***void setup() {
   // put your setup code here, to run once:
   pinMode(3.3, OUTPUT); //plugged into 3.3V
 }
@@ -9,4 +9,78 @@ void loop() {
   delay(2000);
   digitalWrite(3.3, LOW);
   delay(2000);
+}
+***/
+
+
+// PWM pins are 3, 5, 6, 9, 10, 11, 12
+// Master value of how many LEDs I want to have connected
+int const howManyLeds = 5;
+// The analogWrite command goes from 0 to 255
+int const maxBrightness = 255;
+// Which pins on the Arduino UNO support Pulse Witdh Modulation (PWM).
+int pwmPins[] = {3,5,6,9,10,11,12};
+// Create our array of actually used pins with LEDs attached.
+int usedPins[howManyLeds];
+// Each LED/pin has a different fade speed, we keep that speed multiplier in here.
+float currentFadeAmount[howManyLeds];
+// For each pin we are using, we also need a current brightness.
+int currentBrightness[howManyLeds];
+// The counter that we add to, and feed into the sin() function.
+float timeAroundLoop;
+// Number we add to our timeAroundLoop counter.  Make this bigger for faster pulses.
+float timeAdditionValue = 0.005;
+float tempSinVal;
+
+// ------------------------------------------------------------
+
+void setup(){  
+// usedPins is our master destination list of pins.
+  for (int i=0; i < howManyLeds; i++)
+  {
+    // Fill my new array with the available PWM pins.
+    usedPins[i] = pwmPins[i];
+  }
+ // currentFadeAmount is our increment/decrement to our currentBrightness val.
+  for (int i = 0; i < howManyLeds; i++)
+  {
+    // Set the fading speed to be somewhat randomized.
+    currentFadeAmount[i] = random(0.0,15.0);
+  }
+ // Prepare the pins as outputs.  (This may not be needed, but it doesn't hurt.)
+  for (int i = 0; i < howManyLeds; i ++)
+  {
+    pinMode(usedPins[i], OUTPUT);
+  }
+}
+
+// ------------------------------------------------------------
+void loop()
+{
+   // Increment our time counter every loop.
+  timeAroundLoop = timeAroundLoop + timeAdditionValue;
+  for (int i = 0; i < howManyLeds; i++)
+  {
+    // Each LED fades fater or slower than each other - multiply time by speed.
+    tempSinVal = timeAroundLoop*currentFadeAmount[i];
+
+    // This gives us a sinewave value of the adjusted time.  Values from -1 to 1.
+    tempSinVal = sin(tempSinVal);
+
+    // Absolute the sine wave (no negatives) to give a bouncing ball style wave.
+    tempSinVal = abs(tempSinVal);
+
+    // Do 1-value, to turn the bouncing ball upside down.
+    // It now looks like a hanging garland, just touching the ground.
+    tempSinVal = 1-tempSinVal;
+
+    // Set the brightness to this value, multiplied by our max brightness.
+    // The scale now goes from 0-255 instead of 0-1.
+    currentBrightness[i] = tempSinVal * maxBrightness;
+    // Write this value to the pin.
+    analogWrite(usedPins[i], currentBrightness[i]);
+  }
+
+  // I probably don't need to do a delay here, I could just slow down the timeAroundLoop more.
+  delay(30);
 }
