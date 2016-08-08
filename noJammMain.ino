77b6bc341ee936c55a915a551d0797196106e4ae
marison17 <mleversen17@gmail.com>
2016-08-08 16:05:01 -0500

    Update noJammMain.ino

--- noJammMain.ino
+++ noJammMain.ino
@@ -1,98 +1,116 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 #include <Tone.h>
-
-
-// A4=SDA, A5=SCL
-LiquidCrystal lcd(0);
-int PI=3.14;
-unsigned int tovf1 = 0;
-unsigned long frequency = 0;
-unsigned long temp = 0;
-// timer 1 is the counter
-//16-bit counter overflows after 65536 counts
-//tovf1 keeps track of how many times you overflow
-
-// PWM pins are 3, 5, 6, 9, 10, 11, 12
-// Master value of how many LEDs I want to have connected
-int const howManyLeds = 5;
-// The analogWrite command goes from 0 to 255
-int const maxBrightness = 255;
-// Which pins on the Arduino UNO support Pulse Witdh Modulation (PWM).
-int pwmPins[] = {3,5,6,9,10,11,12};
-// Create our array of actually used pins with LEDs attached.
-int usedPins[howManyLeds];
-// Each LED/pin has a different fade speed, we keep that speed multiplier in here.
-float currentFadeAmount[howManyLeds];
-// For each pin we are using, we also need a current brightness.
-int currentBrightness[howManyLeds];
-// The counter that we add to, and feed into the sin() function.
-float timeAroundLoop;
-// Number we add to our timeAroundLoop counter.  Make this bigger for faster pulses.
-float timeAdditionValue = 0.005;
-//float tempSinVal;
+#include <stdint.h>
 
 
 
-ISR(TIMER1_OVF_vect)
-{
-  tovf1++
-}
+
+#define NOTE_A4  440
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+
+
+
+Tone PIEZOPIN1;
+Tone PIEZOPIN2;
+int buttonPin = 0;
+int buttonState = digitalRead(buttonPin);
+
+int Pin6 = 6;       // Led 1
+int Pin9 = 9;       // Led 2 
+
+int Value6 = 0;    //Value Led1
+int Value9 = 0;    //Value Led2
+
+int analog0Pin = 0;      // button to start wave
+int val0 = 0;             // variable to store the value read
+
+int threshold = 512;     // threshold for button
+int timer = 100;
+
 
 void setup() {
-  // usedPins is our master destination list of pins.
-  for (int i=0; i < howManyLeds; i++)
-  {
-    // Fill my new array with the available PWM pins.
-    usedPins[i] = pwmPins[i];
-  }
- // currentFadeAmount is our increment/decrement to our currentBrightness val.
-  for (int i = 0; i < howManyLeds; i++)
-  {
-    // Set the fading speed to be somewhat randomized.
-    currentFadeAmount[i] = random(0.0,15.0);
-  }
- // Prepare the pins as outputs.  (This may not be needed, but it doesn't hurt.)
-  for (int i = 0; i < howManyLeds; i ++)
-  {
-    pinMode(usedPins[i], OUTPUT);
-  }
+  pinMode(10, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  PIEZOPIN1.play(NOTE_A4);
+  PIEZOPIN2.play(NOTE_A4);
+  while(PIEZOPIN1.isPlaying() || PIEZOPIN2.isPlaying());
+ pinMode(analog0Pin, INPUT);
+ pinMode(Pin6, OUTPUT);   // sets the digital pin as output
+ pinMode(Pin9, OUTPUT);   // sets the digital pin as output
+ 
+ Serial.begin(9600);          //  setup serial
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("NOJAMM !!!!");
+}
 
-  // put your setup code here, to run once:
-  pinMode(5, INPUT);
+
   
-  void loop() {
-    
-    
-    
-    
-tone1.play(NOTE_C4);
-tone2.play(NOTE_A4);
-delay(noteDuration);
-tone1.stop();
-tone2.stop();
+void loop() {
+  tone(NOTE_A4, 440, 5000);
+   if (buttonState == HIGH) {
+    //led turn on
+     lcd.print("Now playing: 440 Hz");
+  PIEZOPIN1.play(NOTE_A4);
+  PIEZOPIN2.play(NOTE_A4);
+  while(PIEZOPIN1.isPlaying() || PIEZOPIN2.isPlaying());
+   }
+   else{
+    //led turn off
+    //lcd turn on
+  PIEZOPIN1.play(NOTE_A4);
+  PIEZOPIN2.play(NOTE_A4);
+  while(PIEZOPIN1.isPlaying() || PIEZOPIN2.isPlaying());
+   }
+   { 
+ val0 = analogRead(analog0Pin);    // read the input pin for button
+ Serial.println(val0);             // debug value
 
+if (val0 >= threshold)                // 
+
+{ 
+ for(Value6 = 0 ; Value6 <= 255; Value6+=5) // fade in (from min to max) Led 1
+ { 
+   analogWrite(Pin6, Value6);           // sets the value (range from 0 to 255) 
+   delay(30);                            // waits for 30 milli seconds to see the dimming effect 
+ } 
+ for(Value6= 255; Value6 >=0; Value9-=5)   // fade out (from max to min) 
+ { 
+   analogWrite(Pin6, Value6); 
+   delay(30); 
+     } 
+ 
+ 
+  if (Value6 = 255)             
+
+{ 
+ for(Value9 = 0 ; Value9 <= 255; Value9+=5) // fade in (from min to max) Led 2
+ { 
+   analogWrite(Pin9, Value9);           // sets the value (range from 0 to 255) 
+   delay(30);                            // waits for 30 milli seconds to see the dimming effect 
+ } 
+ for(Value9 = 255; Value9 >=0; Value9-=5)   // fade out (from max to min) 
+ { 
+   analogWrite(Pin9, Value9); 
+   delay(30); 
+     } 
+ 
+     
+     if (Value9 = 255);
+
+
+ }
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis()/1000);
   // Increment our time counter every loop.
-  timeAroundLoop = timeAroundLoop + timeAdditionValue;
-  for (int i = 0; i < howManyLeds; i++)
-  {
-    // Each LED fades fater or slower than each other - multiply time by speed.
-    tempSinVal = timeAroundLoop*currentFadeAmount[i];
+  //timeAroundLoop = timeAroundLoop + timeAdditionValue;
 
-    // This gives us a sinewave value of the adjusted time.  Values from -1 to 1.
-    tempSinVal = sin(tempSinVal);
-
-    // Absolute the sine wave (no negatives) to give a bouncing ball style wave.
-    tempSinVal = abs(tempSinVal);
-
-    // Do 1-value, to turn the bouncing ball upside down.
-    // It now looks like a hanging garland, just touching the ground.
-    tempSinVal = 1-tempSinVal;
-
-    // Set the brightness to this value, multiplied by our max brightness.
-    // The scale now goes from 0-255 instead of 0-1.
-    currentBrightness[i] = tempSinVal * maxBrightness;
-    // Write this value to the pin.
-    analogWrite(usedPins[i], currentBrightness[i]);
-  }
-
+}
+}
+}
